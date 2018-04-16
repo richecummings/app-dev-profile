3ff8a4b4609659b8e100461f0f0d747dc01d6214
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-04-16 17:25:38 -0400

    iPad layout

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var customizeDashboardBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        logoImg.layer.cornerRadius = 10
+        customizeDashboardBtn.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
